f9148a64de62571e0746828a6b747cc61f69d0c2
neilsadev <neiloyhassan1@gmail.com>
2022-09-25 13:18:58 +0600

    lines and depos logic

--- app/main.py
+++ app/main.py
@@ -1,3 +1,7 @@
+MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
+
 def depos():
     while True:
         amount = input("What would you like to deposit? $")
@@ -12,4 +16,23 @@ def depos():
     
     return amount
 
-depos()
+def get_number_of_lines():
+    while True:
+        lines = input("Number of lines you want to bet on (1=" + str(MAX_LINES) + ")? ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print("Enter a valid number of lines. ")
+        else:
+            print("Please enter a number.")
+    
+    return lines
+
+def main():
+    balacnce = depos()
+    lines = get_number_of_lines()
+    print(balacnce, lines)
+
+main()
